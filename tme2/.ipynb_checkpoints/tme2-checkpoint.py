74db8624b4004752be914e6c062d06824e1d9503
hitaw <lea.movsessian@gmail.com>
2023-10-05 09:16:31 +0200

    ajout tme3

---
+++ tme2/.ipynb_checkpoints/tme2-checkpoint.py
@@ -0,0 +1,176 @@
+#Timothe Colin 21206121
+#Lea Movsessian 28624266
+
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+#1 - La planche de Galton
+
+def bernoulli(p):
+    if random.random() <= p:
+        return 1
+    else:
+        return 0
+    
+def binomiale(n,p):
+    X=0
+    for i in range(n):
+        if random.random() <= p:
+            X=X+1
+            
+    return X
+
+
+def galton(l,n,p):
+    
+    tableau = []
+    
+    for i in range(l):
+        tableau.append(binomiale(n,p))
+    return np.array(tableau)
+
+def histo_galton(l,n,p):
+    
+    valeurs = galton(l,n,p)
+    plt.hist (valeurs, np.unique(valeurs))
+
+#2 - Visualisation d'indépendance
+
+def normale (k, sigma):
+    
+    if k%2 == 0:
+        return "Erreur, k pair"
+    
+    x = np.linspace ( -2 * sigma, 2 * sigma, k )
+    
+    norm = []
+    
+    for i in x:    
+        
+        norm.append((1/(sigma*math.sqrt(2*math.pi))) * math.exp(-((i/sigma)**2)/2))
+        
+    return np.array(norm)
+    
+    
+def proba_affine(k,slope):
+    
+    tab = []
+    
+    if k%2 == 0:
+        return "Erreur, k pair"
+    
+    if slope<0:
+        return "Erreur, pente trop faible"
+    
+    if 1/k + (-(k-1)/2)*slope <0:
+        return "Erreur, pente trop élevée"
+    
+    for i in range(k):
+        tab.append(1/k+(i-(k-1)/2)*slope)
+    
+    return np.array(tab)
+
+#3 - 1 - Indépendance de X et T conditionnellement à (Y,Z)
+
+def Pxy(norm, affi):
+    
+    n = np.shape(norm)[0]
+    m = np.shape(affi)[0]
+    mat = [[0 for i in range(n)] for j in range(m)]
+    for i in range(n):
+        for j in range(m):
+            mat[i][j] = norm[i]*affi[j]
+            
+    return np.array(mat)
+
+
+def calcYZ(P_XYZT):
+    
+    mat = [[0 for i in range(2)] for j in range(2)]
+    
+    for y in range(2):
+        for z in range(2):
+            somme = 0
+            for x in range(2):
+                for t in range(2):
+                    somme += P_XYZT[x][y][z][t]
+            
+            mat[y][z] = somme
+            
+    return np.array(mat)
+    
+def calcXTcondYZ(P_XYZT):
+    P_YZ = calcYZ(P_XYZT)
+    mat = [[[[0 for i in range(2)] for j in range(2)] for k in range(2)] for l in range(2)]
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                for t in range(2):
+                    mat[x][y][z][t]=P_XYZT[x][y][z][t]/P_YZ[y][z]
+                    
+    return np.array(mat)
+
+def calcX_etTcondYZ(P_XYZT):
+    P_YZ = calcYZ(P_XYZT)
+    P_XcondYZ = np.zeros((2, 2, 2))
+    P_TcondYZ = np.zeros((2, 2, 2))
+    
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                for t in range(2):
+                    P_XcondYZ[x][y][z] += P_XYZT[x][y][z][t]
+                    P_TcondYZ[t][y][z] += P_XYZT[x][y][z][t]
+    
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                P_XcondYZ[x][y][z] /= P_YZ[y][z]
+                P_TcondYZ[x][y][z] /= P_YZ[y][z]
+                    
+    return P_XcondYZ, P_TcondYZ
+
+def testXTindepCondYZ(P_XYZT, epsilon=1e-10):
+    P_XcondYZ, P_TcondYZ = calcX_etTcondYZ(P_XYZT)
+    P_XTcondYZ = calcXTcondYZ(P_XYZT)
+    
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                for t in range(2):
+                    left_side = P_XcondYZ[x][y][z] * P_TcondYZ[t][y][z]
+                    right_side = P_XTcondYZ[x][y][z][t]
+                    
+                    if abs(left_side - right_side) > epsilon:
+                        return False
+    
+    return True
+
+# 3 - 2 Indépendance de X et Y
+
+def testXindepYZ(P_XYZT, epsilon):
+    P_XYZ = np.zeros((2, 2, 2))
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                for t in range(2):
+                    P_XYZ[x][y][z] += P_XYZT[x][y][z][t]
+    P_X = np.zeros(2)
+    P_YZ = np.zeros((2,2))
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                P_X[x] += P_XYZ[x][y][z]
+                P_YZ[y][z] += P_XYZ[x][y][z]
+                
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                left_side = P_X[x] * P_YZ[y][z]
+                right_side = P_XYZ[x][y][z]
+                    
+                if abs(left_side - right_side) > epsilon:
+                    return False
+    return True
---
+++ tme3/tme3.py
@@ -0,0 +1,118 @@
+#Léa Movsessian 28624266
+#Timothée Colin 21206121
+
+# Apprentissage 
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import pickle as pkl
+
+def learnML_parameters ( X_train,Y_train ):
+    mu = np.zeros((10,256))
+    cmpt_cla = np.zeros(10)
+    sig = np.zeros((10,256))
+    
+    n,d = X_train.shape
+    
+    for image in range(n):
+        classe = Y_train[image]
+        cmpt_cla[classe] += 1
+        for pixel in range(d):
+            mu[classe][pixel] += X_train[image][pixel]
+            
+    
+    for i in range(10):
+        for j in range(256):
+            mu[i][j] /= cmpt_cla[i]
+            
+    for image in range(n):
+        classe = Y_train[image]
+        for pixel in range(d):
+            sig[classe][pixel] += (mu[classe][pixel] - X_train[image][pixel])**2
+            
+    for i in range(10):
+        for j in range(256):
+            sig[i][j] /= cmpt_cla[i]
+            sig[i][j] = math.sqrt(sig[i][j])
+            
+    return mu,sig
+        
+    
+def log_likelihood(X_train_i, mu_i, sig_i, defeps):
+    
+    res = 0
+    
+    for i in range(X_train_i.shape[0]):
+        if sig_i[i] != 0:
+            res += np.log(2*np.pi*sig_i[i]**2) + (X_train_i[i] - mu_i[i])**2/sig_i[i]**2
+        else:
+            if defeps == -1:
+                res += -2
+            else:
+                res += np.log(2*np.pi*defeps**2) + (X_train_i[i] - mu_i[i])**2/defeps**2
+        
+    res *= -1/2
+    
+    return res
+
+def classify_image(X_train_i, mu, sig, eps):
+    
+    classe = 0
+    res = - math.inf
+    
+    for i in range(10):
+        vrai = log_likelihood(X_train_i, mu[i], sig[i], eps)
+        if  vrai >= res:
+            res = vrai
+            classe = i
+            
+    return classe
+
+def classify_all_images(X_train, mu, sig, eps):
+    
+    classe = np.zeros(X_train.shape[0])
+    
+    for i in range(X_train.shape[0]):
+        classe[i] = classify_image(X_train[i], mu, sig, eps)
+        
+    return classe
+
+def matrice_confusion(Y_train, Y_train_hat):
+    
+    mat = np.zeros((10,10))
+    
+    for i in range(Y_train.shape[0]):
+        mat[int(Y_train[i])][int(Y_train_hat[i])] += 1
+            
+    return mat
+
+def classificationRate(Y_train,Y_train_hat):
+    
+    rate = 0
+    n = Y_train.shape[0]
+    
+    mat = matrice_confusion(Y_train, Y_train_hat)
+    
+    for i in range(10):
+        rate += mat[i][i]
+    
+    rate /= n
+    return rate
+
+def classifTest(X_test,Y_test,mu,sig,eps):
+    
+    print("1 - Classify all test images ...")    
+    Y_hat = classify_all_images(X_test, mu, sig, eps)
+    
+    rate = classificationRate(Y_test, Y_hat)
+    print("2 - Classification rate : " + str(rate))
+    
+    print("Matrice de confusion :")
+    mat = matrice_confusion(Y_test, Y_hat)
+    
+    plt.figure(figsize=(3,3))
+    plt.imshow(mat)
+    
+    return np.where(Y_test!=Y_hat)
+    
