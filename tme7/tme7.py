833d9af03d9fc72405510d8b43bfe091d9b13040
hitaw <lea.movsessian@gmail.com>
2023-11-20 23:34:47 +0100

    fin question 5

--- tme7/tme7.py
+++ tme7/tme7.py
@@ -53,24 +53,22 @@ def viterbi(genome_test,Pi,A,B):
             psi[j][t] = np.argmax(best)
 
     #terminaison
-    k = np.argmax(delta[:, -1])
-
-    # Chemin
-    q = []
+    s = np.argmax(delta[:, -1])
+    etats_predits = []
     for i in reversed(range(len(genome_test)-1)):
-        q.append(k)
-        k = psi[int(k), i+1]
+        etats_predits.append(s)
+        s = psi[int(s), i+1]
       
-    return np.array(q[::-1])
+    return np.array(etats_predits[::-1])
 
 def get_and_show_coding(etat_predits,annotation_test):
-    codants_predits = etat_predits[etat_predits!=0]
-    codants_test = [annotation_test[annotation_test!=0]]
-    etat_predits[etat_predits!=0]=1 
-    annotation_test[annotation_test!=0]=1
+    codants_predits = etat_predits.copy()
+    codants_test = annotation_test.copy()
+    codants_predits[codants_predits!=0]=1 
+    codants_test[codants_test!=0]=1
     fig, ax = plt.subplots(figsize=(15,2))
-    ax.plot(etat_predits[100000:200000], label="prediction", ls="--")
-    ax.plot(annotation_test[100000:200000], label="annotation", lw=3, color="black", alpha=.4)
+    ax.plot(codants_predits[100000:200000], label="prediction", ls="--")
+    ax.plot(codants_test[100000:200000], label="annotation", lw=3, color="black", alpha=.4)
     plt.legend(loc="best")
     plt.show()
     return codants_predits, codants_test
@@ -78,4 +76,37 @@ def get_and_show_coding(etat_predits,annotation_test):
 #Evaluation des performances
 
 def create_confusion_matrix(codants_predits, codants_tests):
-    pass
+    confusion_matrix = np.zeros((2,2))
+    for i in range(len(codants_predits)):
+        if codants_predits[i] == 1 and codants_tests[i] == 1:
+            confusion_matrix[0][0] += 1
+        elif codants_predits[i] == 1 and codants_tests[i] == 0:
+            confusion_matrix[0][1] += 1
+        elif codants_predits[i] == 0 and codants_tests[i] == 1:
+            confusion_matrix[1][0] += 1
+        elif codants_predits[i] == 0 and codants_tests[i] == 0:
+            confusion_matrix[1][1] += 1
+    return confusion_matrix
+
+"""
+Il semble y avoir une inversion des Faux Négatifs et des Faux Positifs dans le pdf de l'énoncé. 
+En effet, un faux négatif est ajouté à la case [1][0] et est obtenu quand la prédiction (états_predits) = 0 et que l'annotation (annotation_test) = 1.
+C'est ce qui est fait dans la fonction create_confusion_matrix ci-dessus, pourtant les résultats de l'énoncé indiquent l'inverse.
+"""
+
+"""
+En ce qui concerne les performances, le modèle n'est pas utilisable en tant que tel. En effet, le nombre de faux positifs est bien trop élevé.
+"""
+
+def create_seq(N,Pi,A,B,states,obs):
+    """Retourne une séquence d'observations et d'états générée par le HMM"""
+    etats = np.zeros(N)
+    etats[0] = np.random.choice(states, p=Pi)
+    for i in range(1,N):
+        etats[i] = np.random.choice(states, p=A[int(etats[i-1])])
+
+    obsers = []    
+    for i in range(N):
+        obsers.append(np.random.choice(obs, p=B[int(etats[i])]))
+
+    return etats, np.array(obsers)
